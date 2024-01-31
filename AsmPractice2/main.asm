4c3db9028a255e442b39137d638b6e4825a9d5a2
Tanner Bebb <17tbebb@gmail.com>
2024-01-31 09:10:05 -0800

    Changed the data types from word to dword. Swaped over to the eax and ebx registers. Added lines 26 - 28. All I have done is added 2 values together and the subtracted the same values to set everything back to normal. I did this so that I could view the registers to be sure that what I did worked correctly.

--- AsmPractice2/main.asm
+++ AsmPractice2/main.asm
@@ -10,15 +10,23 @@ ExitProcess proto, dwExitCode:dword
 
 .data
 
-myValue WORD 1000h		;H is for the hexadecimal notation.
+myValue_1 DWORD 1000h
+myValue_2 DWORD 2000h
+myValue_3 DWORD 3000h
+myValue_4 DWORD 4000h
 
 .code
 
 main proc
 
-inc myValue				;myValue = 1001h
-mov bx, myValue			;This will store myValue in the bx register.
-dec bx					;bx = 1000h
+inc myValue_1				;myValue_1 = 1001h
+mov eax, myValue_1			;eax = 1001h
+dec eax						;eax = 1000h
+
+mov ebx, myValue_2			;ebx = 2000h
+ADD eax, ebx				;eax = 3000h
+SUB eax, ebx				;eax = 1000h
+
 
 	invoke ExitProcess, 0
 
